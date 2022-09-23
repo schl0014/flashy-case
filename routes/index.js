d82f4d502414c33930432269262cbd6134dbf29c
Mitchel0058 <goed0058@hz.nl>
2022-09-23 09:27:38 +0200

    template was beter

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,13 @@
 import express from 'express';
-
-import { getAppointments , setAppointment} from '../controllers/appointmentController.js';
-
+import { getAppointments, setAppointment } from '../controllers/appointmentController.js';
 const router = express.Router();
 
-
-
 // routes
-
 router.get('/', (req, res, next) => {
-
   res.json('hi');
-
 });
-
-
 
 router.get('/appointment', getAppointments)
 router.post('/appointment', setAppointment)
-
-
 
 export default router;
